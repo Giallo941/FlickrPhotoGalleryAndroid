bcaf335537ff92bf8dc9b77d7a973ef3b8ee2486
gianmarcocotellessa <g.cotellessa@betacom.it>
2018-04-05 09:58:16 +0200

    First Commit

---
+++ app/src/main/java/com/example/gianmarcocotellessa/flickrphotogallery/GalleryActivity.java
@@ -0,0 +1,13 @@
+package com.example.gianmarcocotellessa.flickrphotogallery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+    }
+}
