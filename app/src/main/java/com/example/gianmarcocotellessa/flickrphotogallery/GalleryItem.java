b68bfce6068c733802cfdbedbd361c8fcb2af018
gianmarcocotellessa <g.cotellessa@betacom.it>
2018-04-05 10:47:24 +0200

    Add GalleryItem class (no errors)

--- app/src/main/java/com/example/gianmarcocotellessa/flickrphotogallery/GalleryItem.java
+++ app/src/main/java/com/example/gianmarcocotellessa/flickrphotogallery/GalleryItem.java
@@ -1,4 +1,30 @@
 package com.example.gianmarcocotellessa.flickrphotogallery;
 
 public class GalleryItem {
+
+    private String mId;
+    private String mSecret;
+    private String mServer;
+    private String mFarm;
+
+    public GalleryItem(String id, String secret, String server, String farm) {
+
+        mId = id;
+        mSecret = secret;
+        mServer = server;
+        mFarm = farm;
+
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public String getUrl() {
+
+        return "http://farm" + mFarm + ".static.flickr.com/" + mServer + "/" + mId + "_" + mSecret + ".jpg";
+
+    }
+
+
 }
